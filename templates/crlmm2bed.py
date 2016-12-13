9d8301eb937a9c446e226c501fcb7a7e36546dd1
Scott Hazelhurst <scott.hazelhurst@wits.ac.za>
2016-12-13 10:15:23 +0200

    Updated crlmm2bed.py

--- templates/crlmm2bed.py
+++ templates/crlmm2bed.py
@@ -15,7 +15,7 @@ from __future__ import print_function
 
 import sys
 import os
-
+import re
 
 # check if we are being called from the command line or as a template
 # from Nextflow. If  as a template from Nextflow, then the nextflow process
@@ -23,7 +23,7 @@ import os
 # NB: only for a Nextflow template -- you could also directly call from a Nextflow
 # script if this script is on the path -- in which case the parameters should be passed
 if len(sys.argv)==0:
-    sys.argv = [sys.argv[0],"$CRLMMCALL","$OUTBASE"]
+    sys.argv = [sys.argv[0],"$CRLMMCALL","$MANIFEST","$OUTBASE"]
 
 max_buffer_size = 1<<20
 
@@ -42,6 +42,21 @@ def produceFam(fin,base):
         outf.write("%s\t%s\t0\t0\t0\t0\n"%(pid,pid))
     outf.close()
 
+def alleles(data):
+    m = re.search(".(.*)/(.*).",data)
+    A = m.group(1)
+    B = m.group(2)
+    return (A,B)
+
+
+def getManifest(mfile):
+    snpdet={}
+    with open(mfile) as f:
+        f.readline()
+        for line in f:
+            data = line.split()
+            snpdet[data[1]]=(alleles(data[3]),data[9],data[10])
+    return snpdet
 
 def checkPlainSNV(data):
        ordinary_snp=True
@@ -51,16 +66,22 @@ def checkPlainSNV(data):
 
 # Given a file in CRLMM format, returns the corresponding PLINK-encoded
 # lines in SNP-major format
-def bufferise(bedf):
+def bufferise(fin,bimf):
     n=0
-    for snp in bedf:
+    for snp in fin:
         n=n+1
         bval=0 # byte we are building up
         i=0      # which 2-bits we're on
         data = snp.rstrip().split()
         # we can only handle biallelic SNPs
         if not checkPlainSNV(data): continue
-        bimf.write("1\t%s\t0\t%s\tA\tC\n"%(data[0].replace('"',''),n))
+        snp = data[0].replace('"','')
+        try:
+            ((A,B),chrom,pos)=snpdet[snp]
+            bimf.write("%s\t%s\t0\t%s\t%s\t%s\n"%(chrom,snp,pos,A,B))
+        except KeyError:
+            print("Warning no manifest for SNP %s "%snp)
+            bimf.write("%s\t%s\t0\t%s\t%s\t%s\n"%(0,snp,0,0,0))
         # go through each person's allele for the current SNP
         for person in data[1:]:
             bval = (bval << 2)|crlmm2bed[person]
@@ -71,29 +92,30 @@ def bufferise(bedf):
         if i!=0: yield bval
 
 
-def produceBed(fin,bed):
-    outf = open("%s.bed"%base,"wb")
-    outf.write(bytearray(magic_number_mode))
+def produceBedBim(fin,base):
+    bedf = open("%s.bed"%base,"wb")
+    bimf = open("%s.bim"%base,"w")
+    bedf.write(bytearray(magic_number_mode))
     buffer=[]
     # get each byte in turn from input  and store in 'buffer'
-    for bval in bufferise(fin):
+    for bval in bufferise(fin,bimf):
          buffer.append(bval)
          # at regular intervals sump buffer to disk
          if len(buffer)==max_buffer_size:
-              outf.write(bytearray(buffer))
+              bedf.write(bytearray(buffer))
               buffer=[]
     # anything at end needs to be dumped to disk
     if len(buffer)>0:
-         outf.write(bytearray(buffer))
-    outf.close()
+         bedf.write(bytearray(buffer))
+    bedf.close()
     bimf.close()
 
-if __name__ = "__main__":                 
+if __name__ == "__main__":                 
     fin = open(sys.argv[1])
-    base = sys.argv[2]
-    bimf = open("%s.bim"%base,"w")
+    snpdet = getManifest(sys.argv[2])
+    base = sys.argv[3]
     produceFam(fin,base)
-    produceBed(fin,base)
+    produceBedBim(fin,base)
 
 
 
