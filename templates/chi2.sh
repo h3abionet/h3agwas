55a87d0fc3959e1ed401611a43cba1b539c515a4
shaze <scott.hazelhurst@wits.ac.za>
2018-06-12 21:04:34 +0200

    bug in chi2.sh

--- templates/chi2.sh
+++ templates/chi2.sh
@@ -3,4 +3,4 @@
 hostname
 
 
-plink --bfile $base $covariate $pheno_cmd --threads $num_assoc_cores --chi2 $perm $adjust --out $base
+plink --bfile $base $covariate $pheno_cmd --threads $num_assoc_cores --assoc $perm $adjust --out $base
