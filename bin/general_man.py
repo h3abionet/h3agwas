b3987f20981a2fc40f9c98088a90a269f029b10f
jeantristanb <jeantristanb@free.fr>
2018-09-05 15:20:20 +0200

    fixed bug in tab affichage for position

--- bin/general_man.py
+++ bin/general_man.py
@@ -110,15 +110,16 @@ delta=0
 colours= ['crimson','blue','green']
 xtick_pos=[]
 xtick_label = []
+result['ps_new']=result[PosEnt]
 for chrom_num, chrom_res in chroms:
     this_chrom = result[ChroEnt]==chrom_num
-    result.loc[this_chrom,PosEnt]+=delta
+    result.loc[this_chrom,'ps_new']+=delta
     old_delta=delta
-    delta = delta + int(chrom_res.tail(1)[PosEnt])
+    delta = delta + int(chrom_res.tail(1)['ps_new'])
     xtick_pos.append((delta+old_delta)/2)
     xtick_label.append(str(chrom_num))
     under_thresh = result[PvalueEnt]<0.005
-    ax.scatter(result.loc[this_chrom & under_thresh, PosEnt],\
+    ax.scatter(result.loc[this_chrom & under_thresh, 'ps_new'],\
                -np.log10(result.loc[this_chrom  & under_thresh,PvalueEnt]),c=colours[chrom_num%3])
     if chrom_num == 9:
        ax.set_xticklabels(xtick_label)
