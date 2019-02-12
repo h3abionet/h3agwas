46dbbe5920e99eebdf1512f57e004280eec45be2
jeantristan <jeantristanb@free.fr>
2019-02-12 21:51:23 +0200

    update all change format for metasoft

--- bin/ma_formatmetasoft.py
+++ bin/ma_formatmetasoft.py
@@ -30,137 +30,114 @@ if len(sys.argv) < 3:
 out=sys.argv[1]
 files=sys.argv[2:]
 
+# MERGE STUDIES
+fout=open(out+'.meta','w')
+#fmap=open(out+'.mmap','w')
+flog=open(out+'.log','w')
+
+
+def GetInfoRsGWAS(rsid, snp,A1Pivot, A2Pivot,CompSE, PosA1Head, PosA2Head, PosBetHead, PosPHead,PosSError) :
+    snp[PosA1Head]=snp[PosA1Head].upper()
+    snp[PosA2Head]=snp[PosA2Head].upper()
+    studyindex=-1
+    pivotstudyindex=-2
+    if PosA1Head : 
+      if snp[PosA1Head] not in vectortorbase:
+         return 'NA NA '
+    if PosA2Head : 
+      if snp[PosA2Head] not in vectortorbase:
+         return 'NA NA '
+    
+    beta=snp[PosBetHead]
+    if CompSE :
+        if float(p)==0.5 :
+              p2=0.499999
+        else :
+           p2=snp[PosPHead]
+        stderr="%f"%abs(float(beta)/stats.norm.ppf(float(p2), loc =0, scale = 1))  #'abs(%s/qnorm(%s/2))'%(beta,p)
+    else :
+         stderr=snp[PosSError]
+    # CHECK ALLELE TO PIVOT
+    if PosA2Head and PosA1Head :
+       if A1Pivot == snp[PosA1Head] and A2Pivot == snp[PosA2Head]:
+          return beta+' '+stderr+' ' 
+       elif A1Pivot == snp[PosA2Head] and A2Pivot == snp[PosA1Head]:
+            # SIMPLE FLIP
+            beta='%f'%(float(beta)*-1)
+       elif A1Pivot == comple[snp[PosA1Head]] and A2Pivot == comple[snp[PosA2Head]]:
+                        # STRAND INCONSIS., BUT GOOD
+            flog.write('FLIP_STRAND %s in study %d\n'%(rsid,studyindex))
+       elif  A1Pivot == comple[snp[PosA2Head]] and A2Pivot == comple[snp[PosA1Head]]:
+             # STRAND INCONSIS., SIMPLE FLIP
+             flog.write('FLIP_STRAND %s in study %d\n'%(rsid,studyindex))
+             beta='%f'%(float(beta)*-1)
+       else:
+            flog.write('EXCLUDE %s due to allele inconsistency: A1:%s A2:%s in study %d but A1:%s A2:%s in study %d\n'
+                                   %(rsid, A1Pivot, A2Pivot, pivotstudyindex,
+                               snp[PosA1Head], snp[PosA2Head], studyindex))
+            return 'NA NA '
+    return beta+' '+stderr+' ' 
+
+
 # READ FILES
-studies=[]
+#studies=[]
 newfileslist=[]
+rsidschar={}
+rsidsinfo={}
+listrsall=set([])
 for f in files:
+    listrsfile=set([])
     study={}
     fin=open(f)
     colnames=fin.readline().split()
+    CompSE=False
+    if SError not in colnames:
+       CompSE=True
+       if PHead not in colnames :
+          print("no Phead in "+ f+" : skip")
+          continue
     if BetHead not in colnames :
        print("no beta in "+ f+" : skip")
        continue
-    if PHead not in colnames :
-       print("no Phead in "+ f+" : skip")
-       continue
     newfileslist.append(os.path.basename(f))
+    PosA1Head=None
+    PosA2Head=None
+    PosPHead=None
+    PosSError=None
+    if A1Head in colnames :
+       PosA1Head=colnames.index(A1Head)
+    if A2Head in colnames:
+       PosA2Head=colnames.index(A2Head)
+    if PHead in  colnames:
+       PosPHead=colnames.index(PHead)
+    if SError in  colnames:
+       PosSError=colnames.index(SError)
+    PosBetHead=colnames.index(BetHead)
+    PosRsHead=colnames.index(rsHead)
     for line in fin:
-        snp={}
-        for (x,y) in zip(colnames,line.split()):
-            snp[x]=y
-        rsid=snp[rsHead]
-        study[rsid]=snp
+        spll=line.split()
+        rsid=spll[PosRsHead]
+        if rsid not in listrsall :
+           listrsall.add(rsid)
+           rsidschar[rsid]=rsid+" "
+           if PosA2Head and PosA1Head:
+             rsidsinfo[rsid]=[0,spll[PosA1Head],spll[PosA2Head]]
+        if rsid not in listrsfile :
+           if not spll[PosA1Head] or  not spll[PosA2Head]:
+              if PosA2Head and PosA1Head:
+                rsidsinfo[rsid][0]=[rsidsinfo[rsid][0] , spll[PosA1Head],spll[PosA2Head]]
+           rsidsinfo[rsid][0]+=1  
+           rsidschar[rsid]+=GetInfoRsGWAS(rsid, spll,rsidsinfo[rsid][1], rsidsinfo[rsid][2],CompSE, PosA1Head, PosA2Head, PosBetHead, PosPHead,PosSError)
+        else :
+           print("rs "+ rsid +" multi times :skip")
     fin.close()
-    studies.append(study)
-    
-# UNION OF SNPS
-allsnps={}
-for study in studies:
-    for rsid, snp in study.items():
-        allsnps[rsid]=snp
 
-# SORT SNPS
-#rsids=sorted(allsnps.keys(), 
-#             key=lambda x:(allsnps[x][ChHead],int(allsnps[x][PsHead])))
+for rsid in listrsall :
+   if rsidsinfo[rsid][0] > 1:
+         fout.write(rsidschar[rsid]+'\n')    
 
 wf=open(out+'.'+'files', 'w') 
 wf.write("\n".join(newfileslist))
 wf.close()
-rsids=allsnps
-
-# MERGE STUDIES
-fout=open(out+'.meta','w') 
-#fmap=open(out+'.mmap','w')
-flog=open(out+'.log','w')  
-for rsid in rsids:
-    output=rsid+'\t'
-    pivot=-1
-    pivotstudyindex=-1
-    numstudy=0
-    for study in studies:
-        studyindex=studies.index(study)+1
-        if rsid in study:
-            snp=study[rsid]
-            snp[A1Head]=snp[A1Head].upper()
-            snp[A2Head]=snp[A2Head].upper()
-            if snp[A1Head] not in vectortorbase  or snp[A1Head] not in vectortorbase :
-               output+='NA NA '
-               continue 
-            if 'OR' in snp:
-                beta='log(%s)'%snp['OR']
-            elif BetHead in snp:
-                beta=snp[BetHead]
-            else:
-                assert 0, 'OR or BETA must be in columns'
-            if PHead in snp:
-                p=snp[PHead]
-            else:
-                assert 0, 'P must be in columns'
-            #if SError in snp :
-            if SError in snp :
-               stderr=snp[SError] 
-            else :
-               if float(p)==0.5 :
-                  p2=0.499999
-               else :
-                  p2=p
-               stderr="%f"%abs(float(beta)/stats.norm.ppf(float(p2), loc =0, scale = 1))  #'abs(%s/qnorm(%s/2))'%(beta,p)
-            if pivot == -1:
-                pivot=snp # 1ST STUDY's SNP INFO IS PIVOT
-                pivotstudyindex=studyindex
-            else:
-                # CHECK ALLELE TO PIVOT
-                if A2Head in pivot and A2Head in snp:
-                    if pivot[A1Head] == snp[A1Head] and \
-                       pivot[A2Head] == snp[A2Head]:
-                        # GOOD
-                        pass
-                    elif pivot[A1Head] == snp[A2Head] and \
-                         pivot[A2Head] == snp[A1Head]:
-                        # SIMPLE FLIP
-                        beta='%f'%(float(beta)*-1)
-                    elif pivot[A1Head] == comple[snp[A1Head]] and \
-                         pivot[A2Head] == comple[snp[A2Head]]:
-                        # STRAND INCONSIS., BUT GOOD
-                        flog.write('FLIP_STRAND %s in study %d\n'%(rsid,studyindex))
-                    elif pivot[A1Head] == comple[snp[A2Head]] and \
-                         pivot[A2Head] == comple[snp[A1Head]]:
-                        # STRAND INCONSIS., SIMPLE FLIP
-                        flog.write('FLIP_STRAND %s in study %d\n'%(rsid,studyindex))
-                        beta='%f'%(float(beta)*-1)
-                    else:
-                        flog.write('EXCLUDE %s due to allele inconsistency: A1:%s A2:%s in study %d but A1:%s A2:%s in study %d\n'
-                                   %(rsid, pivot[A1Head], pivot[A2Head], pivotstudyindex,
-                                     snp[A1Head], snp[A2Head], studyindex))
-                else: 
-                    if pivot[A1Head] == snp[A1Head]:
-                        # GOOD
-                        pass
-                    else:
-                        flog.write('EXCLUDE %s due to allele inconsistency: A1:%s in study %d but A1:%s in study %d\n'
-                                   %(rsid, pivot[A1Head], pivotstudyindex,
-                                     snp[A1Head], studyindex))
-                # CHECK CHR & BP TO PIVOT
-                #if pivot[ChHead] != snp[ChHead]:
-                #    flog.write('WARNING %s has chr inconsistency\n'%rsid)
-                #if pivot[PsHead] != snp[PsHead]:
-                #    flog.write('WARNING %s has basepair inconsistency\n'%rsid)
-            output+=beta+' '+stderr+' '
-            numstudy+=1
-        else:
-            output+='NA NA '
-    if numstudy == 1:
-        flog.write('EXCLUDE %s due to being in single study\n'%rsid)
-    else:
-        fout.write(output+'\n')
-    #    fmap.write('%s\t%s\t%s\t%s\t%d\n'%
-    #               (rsid, pivot[ChHead], pivot[PsHead], pivot[A1Head], numstudy))
-fout.close()
-#fmap.close()
-flog.close()
-
-# CALL R TO EVALUATE MATH EXPRESSION
-#subprocess.call(['R --vanilla --slave "--args '+out+'.meta.tmp '+out+'.meta" < plink2metasoft_subroutine.R'],shell=True)
-#subprocess.call(['rm '+out+'.meta.tmp'], shell=True)
 
 
