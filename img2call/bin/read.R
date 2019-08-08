e2133d890a5992f90047de35859ec32f85db715b
shaze <scott.hazelhurst@wits.ac.za>
2019-08-08 13:37:02 +0200

    Change of reorg and minor fix

--- img2call/bin/read.R
+++ img2call/bin/read.R
@@ -3,25 +3,29 @@ library(crlmm)
 library(ff)
 library(readxl)
 
-sample=read_excel("../aux/dna.xlsx")
+sample=read_excel("../data/testbatch.xlsx")
 
 print("Read sample sheet")
 names = paste(sample$"Array Info.S",sample$"Sentrix ID",sep="_")
 
-names=c("202351420042_R01C01",  "202351420042_R02C01", "202351420042_R03C01",    "202351420042_R04C01",  "202351420042_R05C01", "202351420042_R06C01", "202351420042_R08C01", "202351420049_R01C01", "202351420049_R02C01", "202351420049_R03C01", "202351420049_R04C01","202351420049_R05C01","202351420049_R06C01",  "202351420049_R07C01")
 
-annof =read.table("../aux/A3.annot",sep=",")
+
+annof =read.table("../aux/A3.annot",sep=",",header=TRUE)
 print("Read annotation file")
 
-
-
+path="/spaces/scott/h3agwas/img2call/data/all"
+anno=annof
+arrayNames=names
+arrayInfoColNames=list(barcode="Array Info.S", position="Sentrix ID")
+sep="_"
+fileExt=list(green="Grn.idat", red="Red.idat")
 
 sample$"Sample_ID" = sample$"Institute Sample Label"
-sample = sample[ , !(names(sample) %in% drops)]
+
 
 xx =
      genotype.Illumina(sampleSheet=sample,
-     path="/spaces/scott/h3agwas/img2call/aux/idats/",
+     path="/spaces/scott/h3agwas/img2call/data/all",
      arrayNames = names,                  
      arrayInfoColNames=list(barcode="Array Info.S", position="Sentrix ID"),
      highDensity=TRUE, sep="_",
