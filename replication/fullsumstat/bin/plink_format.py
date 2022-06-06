50be9b9ac91889aca35d0e40271993dbcd431b26
jeantristanb <jeantristanb@free.fr>
2022-06-06 12:01:47 +0200

    update

--- replication/fullsumstat/bin/plink_format.py
+++ replication/fullsumstat/bin/plink_format.py
@@ -1,97 +1,296 @@
 #!/usr/bin/env python3
-''' 
-format file for gcta, append freq and n if need and bfile
-'''
-from  subprocess import CalledProcessError
-import subprocess
-import os
 import argparse
-import numpy as np
-import pandas as pd
+from math import sqrt
+import os
 import sys
+from scipy import stats
+EOL=chr(10)
+def formatrs(chro, bp,a1,a2) :
+  a1=a1.upper()
+  a2=a2.upper()
+  if a1 > a2 :
+     AA1=a1
+     AA2=a2
+  else :
+     AA1=a2
+     AA2=a1
+  newrs=chro+'_'+bp+'_'+AA1+'_'+AA2
+  return(newrs)
 
-EOL = chr(10)
-#SNP A1 A2 freq b se p N 
+def extractinfobim(bimfile):
+   readbim=open(bimfile)
+   dicrskey={}
+   dickeyrs={}
+   listkey=set([])
+   listchrbp=set([])
+   listdup=set([])
+   for line in readbim :
+      splline=line.replace('\n','').split()
+      chro=splline[0]
+      rs=splline[1]
+      bp=splline[3]
+      a1=splline[4]
+      a2=splline[5]
+      keyposalt=formatrs(chro, bp,a1,a2)
+      keychr=chro+':'+bp
+      if keychr in listchrbp :
+         listdup.add(keychr)
+      else :
+         listchrbp.add(keychr)
+      listkey.add(keyposalt) 
+      dicrskey[rs]=keyposalt
+      dickeyrs[keyposalt]=rs
+   return (listkey, listdup, dicrskey,dickeyrs)
+
+def readsummarystat(sumstat, dickeyrs,listdup, listkey, rs_header,n_header, chr_header, bpval_header, beta_header, z_header, a1_header, a2_header, se_header, af_header, pval_header,n_value, maf, nlim, keep_genet) :
+    def getposheader( head, headersumstat):
+        if head is not None:
+         return headersumstat.index(head)
+        return None
+    def gv(infogwas, posheader, othervalue="NA", formatfct=str) :
+       if posheader is not None:
+          val=infogwas[posheader]
+          try :
+            return formatfct(val)
+          except :
+            return othervalue 
+       return othervalue
+    readsummstat=open(sumstat)
+    headersumstat=readsummstat.readline().replace('\n','').split()
+    n_headerp=getposheader(n_header, headersumstat)
+    se_headerp=getposheader(se_header, headersumstat)
+    a2_headerp=getposheader(a2_header, headersumstat)
+    a1_headerp=getposheader(a1_header, headersumstat)
+    z_headerp=getposheader(z_header, headersumstat)
+    beta_headerp=getposheader(beta_header, headersumstat)
+    bpval_headerp=getposheader(bpval_header, headersumstat)
+    chr_headerp=getposheader(chr_header, headersumstat)
+    rs_headerp=getposheader(rs_header, headersumstat)
+    af_headerp=getposheader(af_header, headersumstat)
+    pval_headerp=getposheader(pval_header, headersumstat)
+    if maf is not None:
+      mafupper= 1 - maf
+    if n_value is None:
+       n_value_default="NA"
+    dicres={}
+    listnewkey=set([])
+    balisen=(nlim  is not None) and (n_headerp is not None)
+    for line in readsummstat :
+        splline=line.replace('\n','').split()
+        key=formatrs(splline[chr_headerp],splline[bpval_headerp],splline[a1_headerp],splline[a2_headerp])
+        key2=splline[chr_headerp]+':'+splline[bpval_headerp]
+        balisersbim=key in dickeyrs
+        if (key2 not in listdup)  and (keep_genet==0 or balisersbim):
+           freq=gv(splline,af_headerp)
+           balise=True
+           if freq!="NA" and (maf is not None):
+             freq=float(freq)
+             balise=freq>=maf and freq<=mafupper
+           if balisen and balise:
+             Nval=float(splline[n_headerp])
+             balise=balise and Nval > nlim
+           if balise :
+             # 0 SNPBim, 1 SNPSumStat, 2 CHR, 3 BP, 4 A1, 5 A2, 6 Z, 7 BETA, 8 SE, 9 AF, 10 N, P 11 
+             rsbim="NA"
+             if balisersbim==False:
+                keybim='NA'
+             else :
+                 keybim=dickeyrs[key]
+             dicres[key]=[keybim,gv(splline, rs_headerp, key), gv(splline, chr_headerp), gv(splline, bpval_headerp), gv(splline, a1_headerp).upper(), gv(splline, a2_headerp).upper(), gv(splline, z_headerp,formatfct=float), gv(splline, beta_headerp,formatfct=float), gv(splline, se_headerp,formatfct=float), gv(splline,af_headerp,formatfct=float), gv(splline, n_headerp, n_value_default,formatfct=float), gv(splline, pval_headerp, line,formatfct=float)]  
+             listnewkey.add(key)
+    return (dicres, listnewkey)
+
+def ExtractFreqN(bfile, dicsumstat, listkey,dicbim,freq_header,rs_header, n_header, chr_header,bpval_header, bin_plk, keep, threads, memory) :
+   if (n_header or nvalue) and (freq_header is not None):
+       return (dicsumstat, listkey)
+   plkfreqfil=os.path.basename(bfile)
+   out_range="tmp.frq"
+   if bfile is None :
+     print("no header for n or freq and bfile")
+     sys.exit()
+   Cmd=bin_plk+" -bfile "+bfile+" --freq --keep-allele-order "
+   rangelist=out_range+".bed"
+   writebed=open(rangelist, 'w') 
+   [writebed.write("\t".join([dicsumstat[key][2],dicsumstat[key][3], dicsumstat[key][4], dicsumstat[key][0]])+'\n') for key in listkey]
+   writebed.close()
+   if keep is not None:
+     Cmd+=" --keep "+keep
+   Cmd+=" --threads "+str(threads)
+   Cmd+=" --extract range "+rangelist
+   Cmd+=" --memory "+memory
+   Cmd+=" --out "+plkfreqfil
+   os.system(Cmd)
+   data_n=pd.read_csv(plkfreqfil+".frq",delim_whitespace=True)
+   readfreq=open(plkfreqfil+".frq")
+   #CHR	Chromosome code
+   #SNP	Variant identifier
+   #A1	Allele 1 (usually minor)
+   #A2	Allele 2 (usually major)
+  #MAF	Allele 1 frequency
+  #NCHROBS	Number of allele observations
+   header=readfreq.replace('\n', '').split()
+   posrs=0
+   posa1=1
+   posa2=2
+   posaf=3
+   posn=4
+   if maf is not None:
+      mafupper= 1 - maf
+   newlistkey=set([])
+   for line in readfreq :
+      splline=line.replace('\n', '').split()
+      key=dicbim[splline[0]]
+      # 0 SNP1, 1 SNP2, 2 CHR, 3 BP, 4 A1, 5 A2, 6 Z, 7 BETA, 8 SE, 9 AF, 10 N, P 11 
+      if not freq_header : 
+        af=float(splline[posaf])
+        balise=True
+        if maf is not None:
+           balise=af>=maf and af<=mafupper
+        if splline[posa1]==dicsumstat[key][5] and splline[posa2]==dicsumstat[key][4] :
+          af = 1 - af
+        if balise :
+          dicsumstat[key][9]=af
+          newlistkey.add(key)
+        else :
+          print("key "+key)
+          del dicsumstat[key]
+      if not n_header : 
+         dicsumstat[key][10]=int(splline[posn])/2
+   return (dicsumstat,newlistkey)
+
+def addz(dicsumstat, listkey ,z_header) :
+ # 0 SNP1, 1 SNP2, 2 CHR, 3 BP, 4 A1, 5 A2, 6 Z, 7 BETA, 8 SE, 9 AF, 10 N, P 11 
+  if z_header :
+     return dicsumstat
+  for key in listkey :
+      dicsumstat[key][6]=float(dicsumstat[key][7])/float(dicsumstat[key][8])
+  return dicsumstat
+ 
+def addbetase(dicsumstat, listkey ,beta_header, se_header) :
+   if beta_header and se_header :
+       return dicsumstat
+   for key in listkey :
+    z, p, n=float(dicsumstat[key][6]),float(dicsumstat[key][9]),int(dicsumstat[key][10])
+    if z!='NA' and p!='NA' and n!='NA':
+     b=z/sqrt(2*p*(1-p)*(n+z**2))
+     se=1/sqrt(2*p*(1-p)*(n+z**2))
+     dicsumstat[key][7]=b
+     dicsumstat[key][8]=se
+    else :
+      print("pos "+key+" : z "+str(z)+" p : "+str(p)+" n "+str(n)+" is na")
+   return(dicsumstat)
+
+def compute_usingp(dicsumstat, listkey,used_p, beta_header, se_header, z_header) :
+  if args.used_p==0 :
+    return(dicsumstat)
+  for key in listkey :
+    if dicsumstat[key][7]!='NA':
+      if float(dicsumstat[key][7])> 0 :
+        sens=1
+      else :
+        sens = -1 
+    elif dicsumstat[key][6]!='NA' :
+      if float(dicsumstat[key][6]) > 0 :
+        sens = 1
+      else :
+        sens = -1
+    else :
+      print("oos "+ key+" z and beta NA")
+      continue
+    dicsumstat[key][6]=abs(stats.norm.ppf(1-dicsumstat[key][11]/2))*sens
+    z, p, n=float(dicsumstat[key][6]),float(dicsumstat[key][9]),int(dicsumstat[key][10])
+    b=z/sqrt(2*p*(1-p)*(n+z**2))
+    se=1/sqrt(2*p*(1-p)*(n+z**2))
+    dicsumstat[key][7]=b
+    dicsumstat[key][8]=se
+  return dicsumstat
+
+def checkrs(dicsumstat, listkey) :
+   for key in listkey :
+      #0 SNP1, 1 SNP2, 2 CHR, 3 BP, 4 A1, 5 A2, 6 Z, 7 BETA, 8 SE, 9 AF, 10 N, P 11 
+      if dicsumstat[key][0]=='NA' :
+         dicsumstat[key][0]=dicsumstat[key][1]
+   return dicsumstat
+
+
+
+
+#    plink_format.py --inp_asso $filegwas --chro_header ${params.head_chr_sumstat1} --bpval_header ${params.head_bp_sumstat1} --a1_header ${params.head_A1_sumstat1} --a2_header ${params.head_A2_sumstat1}   --pval_header ${params.head_pval_sumstat1}  --out ${out} --rs_header ${params.head_rs_sumstat1} $se $beta $z
+
+
 def parseArguments():
-    parser = argparse.ArgumentParser(description='fill in missing bim values')
-    parser.add_argument('--inp_asso',type=str,required=True, help="association files")
-    parser.add_argument('--out', type=str,help="out of tex file",default="test.tex")
-    parser.add_argument('--rs_header',type=str,required=True,help="rs header in inp files")
-    parser.add_argument('--pval_header',type=str,required=True,help="pvalue header in inp files")
-    parser.add_argument('--freq_header',type=str,required=False,help="freq header in inp files",default=None)
-    parser.add_argument('--a1_header',type=str,required=True,help="a1 header in inp files")
-    parser.add_argument('--a2_header',type=str,required=True,help="a2 header in inp files")
-    parser.add_argument('--se_header',type=str,required=True,help="se header in inp files")
-    parser.add_argument('--n_header',type=str,help="n header in inp files", default=None)
-    parser.add_argument('--chr',type=str,help="n header in inp files", default=None)
-    parser.add_argument('--chro_header',type=str,required=True,help="n header in inp files")
-    parser.add_argument('--bp_header',type=str,help="bp header")
-    parser.add_argument('--beta_header',type=str,required=True,help="beta header in inp files")
-    parser.add_argument('--chr_pos',type=str,required=False,help="beta header in inp files")
+    parser = argparse.ArgumentParser(description='extract rs in gwas file')
+    parser.add_argument('--inp_asso',type=str,required=True)
+    parser.add_argument('--rs',type=str,required=False, help="list rs", default="")
+    parser.add_argument('--chro_header',type=str,required=True,help="chro header in inp files")
+    parser.add_argument('--n_header',type=str,required=False,help="chro header in inp files")
+    parser.add_argument('--bp_header',type=str,required=True,help="pos header in inp files")
+    parser.add_argument('--beta_header',type=str,required=False,help="beta header in inp files")
+    parser.add_argument('--z_header',type=str,required=False,help="Z header in inp files")
+    parser.add_argument('--se_header',type=str,required=False,help="se header in inp files")
+    parser.add_argument('--pval_header',type=str,required=True,help="p-value header in inp files")
+    parser.add_argument('--rs_header',type=str,required=False,help="beta header in inp files")
+    parser.add_argument('--n_lim',type=int,required=False,help="beta header in inp files", default=0)
+    parser.add_argument('--a1_header',type=str,required=True,help="beta header in inp files")
+    parser.add_argument('--keep_genet',type=str,help="beta header in inp files", default=0)
+    parser.add_argument('--a2_header',type=str,required=True,help="beta header in inp files")
+    parser.add_argument('--freq_header',type=str,required=False,help="frequencies header in inp files")
+    parser.add_argument('--maf',type=float,default=0.0,help="minor allele frequencies")
+    parser.add_argument('--bin_plk',type=str,required=False,help="plink binary", default="plink")
+    parser.add_argument('--bfile',type=str,required=False,help="bfile if need to compute frequency or N", default=None)
+    parser.add_argument('--keep',type=str,required=False,help="file of data used for if need to compute frequency or N", default=None)
+    parser.add_argument('--used_p',type=int,required=False,help="file of data used for if need to compute frequency or N", default=0)
+    parser.add_argument('--threads',type=int,required=False,help="", default=1)
+    parser.add_argument('--memory',type=int,required=False,help="", default="1000")
+    parser.add_argument('--n',required=False, help="bim file ")
+    parser.add_argument('--out',type=str,default="out",help="b)")
     args = parser.parse_args()
     return args
 
 args = parseArguments()
-inp     = args.inp_asso
 
-rs_head=args.rs_header
-beta_head=args.beta_header
-freq_head=args.freq_header
-pval_head=args.pval_header
-se_head=args.se_header
-a1_head=args.a1_header
-a2_head=args.a2_header
-se_head=args.se_header
-n_head=args.n_header
-IsFreq=False
-IsN=False
-if freq_head :
-  IsFreq=True
-if n_head :
-  IsN=True
+## keep information bim bam
+print(" reading bfile :begin")
+#(listkey, listdup, dicrskey,dickeyrs)
+(listkey, listdup, dicrskey,dickeyrs)=extractinfobim(args.bfile+".bim")
+print(len(listkey))
+print(" reading bfile : end")
+print(" reading sumstat: begin")
+#def readsummarystat(sumstat, dickeyrs,listdup, listkey, rs_header,n_header, chr_header, bpval_header, beta_header, z_header, a1_header, a2_header, se_header, af_header, pval_header,n_value, maf) :
+(dicsumstat,listkey)=readsummarystat(args.inp_asso, dickeyrs,listdup, listkey, args.rs_header, args.n_header, args.chro_header, args.bp_header, args.beta_header, args.z_header, args.a1_header, args.a2_header, args.se_header, args.freq_header, args.pval_header,args.n, args.maf, args.n_lim ,args.keep_genet) 
+print(len(listkey))
+print(" reading sumstat: end")
+print(" reading add N and freq: begin")
+(dicsumstat,listkey)=ExtractFreqN(args.bfile, dicsumstat, listkey,dicrskey,args.freq_header,args.rs_header, args.n_header, args.chro_header,args.bp_header, args.bin_plk, args.keep, args.threads,args.memory)
+print(len(listkey))
+print(" reading add N and freq: End")
+print(" reading add beta se")
+dicsumstat=addbetase(dicsumstat, listkey ,args.beta_header, args.se_header)
+print(" reading add beta se :end")
+dicsumstat=addz(dicsumstat, listkey ,args.z_header)
+dicsumstat=compute_usingp(dicsumstat, listkey,args.used_p, args.beta_header, args.se_header, args.z_header)
+dicsumstat=checkrs(dicsumstat, listkey)
 
-## first step : if
-result = pd.read_csv(inp,delim_whitespace=True)
-result[args.chro_header] = result[args.chro_header].astype(str)
+writeplink=open(args.out+".plink",'w')
+#writelz=open(args.out+".lz",'w')
+#writegcta=open(args.out+".gcta",'w')
+#0 SNP1, 1 SNP2, 2 CHR, 3 BP, 4 A1, 5 A2, 6 Z, 7 BETA, 8 SE, 9 AF, 10 N, P 11 
+# 0 SNPBim, 1 SNPSumStat, 2 CHR, 3 BP, 4 A1, 5 A2, 6 Z, 7 BETA, 8 SE, 9 AF, 10 N, P 11 
+headplink=["SNPKEY","SNP","SNPSS","CHR","BP","A1","A2", "Z","BETA","SE", "FRQ","N","P"]
+headgcta=["SNPKEY","SNP","SNPSS","chr","bp","A1","A2", "z","b","se", "freq","N","p"]
+#["#CHROM","BEGIN","END","MARKER_ID","PVALUE"]
+headlz=["#CHROM","BEGIN","END","MARKER_ID","PVALUE"]
 
-if args.chr :
-   result=result[result[args.chro_header]==args.chr]
+writeplink.write("\t".join(headplink)+"\n")
+#writegcta.write("\t".join(headgcta)+"\n")
 
-if args.chr_pos :
-   chrpos= pd.read_csv(args.chr_pos,delim_whitespace=True,header=None)
-   chrpos.columns=[args.chro_header, args.bp_header]
-   chrpos[args.chro_header] = chrpos[args.chro_header].astype(str)
-   result=pd.merge(chrpos,  result, on=[args.chro_header, args.bp_header])
-   
+#writelz.write("\t".join(headlz)+"\n")
+for key in listkey :
+    info=dicsumstat[key]
+    strdata=[str(x) for x in info]
+    writeplink.write(key+"\t"+"\t".join(strdata)+"\n")
+    #writegcta.write(key+"\t"+"\t".join(strdata)+"\n")
+    #writelz.write("\t".join([strdata[2],strdata[3],strdata[3],strdata[1],strdata[11]])+"\n")
 
-
-#Head=[rs_head, a1_head,a2_head,beta_head, se_head,pval_head]
-#NewHead=["SNP","A1","A2","b","se","p"]
-NewHead=["SNP"]
-Head=[rs_head]
-NewHead.append("CHR")
-Head.append(args.chro_header)
-
-NewHead.append("BP")
-Head.append(args.bp_header)
-
-NewHead+=["A1","A2"]
-Head+=[a1_head,a2_head]
-
-NewHead+=["BETA","SE"]
-Head+=[beta_head, se_head]
-if pval_head :
-   NewHead+=["P"]
-   Head+=[pval_head]
-
-if IsN :
-   Head.append(n_head)
-   NewHead.append("N")
-
-result=result.loc[:,Head]
-result.columns=NewHead
-
-
-result.to_csv(args.out,sep=" ",header=True,index=False,na_rep="NA")
 
 
--- replication/fullsumstat/main.nf
+++ replication/fullsumstat/main.nf
@@ -23,8 +23,8 @@ import java.nio.file.Paths
 def helps = [ 'help' : 'help' ]
 allowed_params = ["cut_maf", "output_dir", "pb_around_rs", "mem_req", "work_dir","mem_req","big_time", "output","nb_cpu" , "input_dir","input_pat", "file_gwas", "gwas_cat", "site_wind", "min_pval_clump", "size_win_kb","secret-key", "region", "AMI", "instanceType","bootStorageSize", "boot-storage-size", "maxInstances", "max-instances", "plink_mem_req", "plink_mem_req"]
 allowed_params_other=["max_forks", "strandreport", "manifest", "idpat", "accessKey", "access-key", "secretKey", "secret-key","region", "AMI","maxInstances", "instanceType", "bootStorageSize", "boot-storage-size", "max-instances", "sharedStorageMount", "shared-storage-mount", "scripts", "max_plink_cores"]
-allowed_params_head_sumstat1 = ["file_gwas_sumstat1","head_pval_sumstat1", "head_freq_sumstat1", "head_bp_sumstat1", "head_chr_sumstat1", "head_rs_sumstat1", "head_beta_sumstat1", "head_se_sumstat1", "head_A1_sumstat1", "head_A2_sumstat1", "head_n_sumstat1", "n_count1"]
-allowed_params_head_sumstat2 = ["file_gwas_sumstat2","head_pval_sumstat2", "head_freq_sumstat2", "head_bp_sumstat2", "head_chr_sumstat2", "head_rs_sumstat2", "head_beta_sumstat2", "head_se_sumstat2", "head_A1_sumstat2", "head_A2_sumstat2", "head_n_sumstat2", 'n_count2']
+allowed_params_head_sumstat1 = ["file_gwas_sumstat1","head_pval_sumstat1", "head_freq_sumstat1", "head_bp_sumstat1", "head_chr_sumstat1", "head_rs_sumstat1", "head_beta_sumstat1", "head_se_sumstat1", "head_A1_sumstat1", "head_A2_sumstat1", "head_n_sumstat1", "n_count1",'head_z_sumstat1']
+allowed_params_head_sumstat2 = ["file_gwas_sumstat2","head_pval_sumstat2", "head_freq_sumstat2", "head_bp_sumstat2", "head_chr_sumstat2", "head_rs_sumstat2", "head_beta_sumstat2", "head_se_sumstat2", "head_A1_sumstat2", "head_A2_sumstat2", "head_n_sumstat2", 'n_count2','head_z_sumstat2']
 allowed_params_clump= []
 
 
@@ -80,7 +80,7 @@ params.head_beta_sumstat1=""
 params.head_se_sumstat1=""
 params.head_A1_sumstat1="ALLELE1"
 params.head_A2_sumstat1="ALLELE0"
-params.head_ncount_sumstat1=""
+params.head_n_sumstat1=""
 
 
 params.file_gwas_sumstat2=""
@@ -93,7 +93,7 @@ params.head_beta_sumstat2=""
 params.head_se_sumstat2=""
 params.head_A1_sumstat2=""
 params.head_A2_sumstat2=""
-params.head_ncount_sumstat2=""
+params.head_n_sumstat2=""
 
 
 
@@ -136,9 +136,9 @@ System.exit(-1)
 filegwas_chrsum2_epos=Channel.fromPath(params.file_gwas_sumstat2,checkIfExists:true)
 process extract_possumstat2{
   input:
-    file(gwas) from filegwas_chrsum2_epos
+    path(gwas) from filegwas_chrsum2_epos
   output:
-    file(out) into sumstat2_bed
+    path(out) into sumstat2_bed
   script :
     out=gwas+".bed"
     """
@@ -154,53 +154,98 @@ error("\n\n------\nError in your config\nFile file_gwas_sumstat2 not defined\n\n
 }
 filegwas_sum1=Channel.fromPath(params.file_gwas_sumstat1,checkIfExists:true)
 
-process extractgwas_forplink{
-   input : 
-     file(filegwas) from filegwas_sum1
-   publishDir "${params.output_dir}/gwas_sub",  overwrite:true, mode:'copy'
-   output :
-     file("$out") into (file_plk_ss1_rep, file_plk_ss1)
-   script :
-    out=filegwas+".assoc"
-    """
-    plink_format.py --inp_asso $filegwas --chro_header ${params.head_chr_sumstat1} --bp_header ${params.head_bp_sumstat1} --a1_header ${params.head_A1_sumstat1} --a2_header ${params.head_A2_sumstat1}   --pval_header ${params.head_pval_sumstat1} --beta_header ${params.head_beta_sumstat1}  --out ${out} --rs_header ${params.head_rs_sumstat1} --se_header ${params.head_se_sumstat1}
-    """
-}
-
 bed = Paths.get(params.input_dir,"${params.input_pat}.bed").toString()
 bim = Paths.get(params.input_dir,"${params.input_pat}.bim").toString()
 fam = Paths.get(params.input_dir,"${params.input_pat}.fam").toString()
 raw_src_ch= Channel.create()
+raw_src_ch2= Channel.create()
 
 Channel
     .from(file(bed),file(bim),file(fam))
     .buffer(size:3)
     .map { a -> [checker(a[0]), checker(a[1]), checker(a[2])] }
-    .set {raw_src_ch}
+   .set {raw_src_ch}
 
+Channel
+    .from(file(bed),file(bim),file(fam))
+    .buffer(size:3)
+    .map { a -> [checker(a[0]), checker(a[1]), checker(a[2])] }
+   .set {raw_src_ch2}
+
+Channel
+    .from(file(bed),file(bim),file(fam))
+    .buffer(size:3)
+    .map { a -> [checker(a[0]), checker(a[1]), checker(a[2])] }
+   .set {raw_src_ch3}
 plk_ch_clump  = Channel.create()
 plk_ch_ld  = Channel.create()
 plk_ch_clumpstat  = Channel.create()
 
-    /*JT : append boltlmm_assoc_ch and a]*/
+
+process extractgwas_forplink{
+   input : 
+     path(filegwas) from filegwas_sum1
+     tuple path(bed), path(bim), path(fam) from raw_src_ch2
+   publishDir "${params.output_dir}/gwas_sub",  overwrite:true, mode:'copy'
+   output :
+     path("${out}.plink") into (file_plk_ss1_rep, file_plk_ss1, fileformat_plk1)
+   script :
+    out=filegwas+".assoc"
+    se=(params.head_se_sumstat1=='') ? "" : " --se_header ${params.head_se_sumstat1} "
+    beta=(params.head_se_sumstat1=='') ? "" : " --beta_header ${params.head_beta_sumstat1} "
+    z=(params.head_z_sumstat1=='') ? "" : " --z_header ${params.head_z_sumstat1} "
+    bfile=bed.baseName
+    headn=(params.head_n_sumstat1=='') ? "" :  "  --n_header ${params.head_n_sumstat1} "
+    ncount=(params.n_count1=='') ? "" :  "  --n ${params.n_count1} "
+    freq=(params.head_freq_sumstat1=='') ? "" :  " --freq_header  ${params.head_freq_sumstat1} "
+    """
+    plink_format.py --inp_asso $filegwas --chro_header ${params.head_chr_sumstat1} --bp_header ${params.head_bp_sumstat1} --a1_header ${params.head_A1_sumstat1} --a2_header ${params.head_A2_sumstat1}   --pval_header ${params.head_pval_sumstat1}  --out ${out} --rs_header ${params.head_rs_sumstat1} $se $beta $z  --bfile  $bfile $headn $ncount $freq
+    """
+}
+
+
+filegwas_sum2=Channel.fromPath(params.file_gwas_sumstat2,checkIfExists:true)
+process extractgwas2_forplink{
+   input : 
+     path(filegwas) from filegwas_sum2
+     tuple path(bed), path(bim), path(fam) from raw_src_ch3
+   publishDir "${params.output_dir}/gwas_sub",  overwrite:true, mode:'copy'
+   output :
+     path("${out}.plink") into fileformat_plk2
+   script :
+    out=filegwas+".assoc"
+    se=(params.head_se_sumstat2=='') ? "" : " --se_header ${params.head_se_sumstat2} "
+    beta=(params.head_se_sumstat2=='') ? "" : " --beta_header ${params.head_beta_sumstat2} "
+    z=(params.head_z_sumstat2=='') ? "" : " --z_header ${params.head_z_sumstat2} "
+    bfile=bed.baseName
+    headn=(params.head_n_sumstat2=='') ? "" :  "  --n_header ${params.head_n_sumstat2} "
+    ncount=(params.n_count2=='') ? "" :  "  --n ${params.n_count2} "
+    freq=(params.head_freq_sumstat2=='') ? "" :  " --freq_header  ${params.head_freq_sumstat2} "
+    """
+    plink_format.py --inp_asso $filegwas --chro_header ${params.head_chr_sumstat2} --bp_header ${params.head_bp_sumstat2} --a1_header ${params.head_A1_sumstat2} --a2_header ${params.head_A2_sumstat2}   --pval_header ${params.head_pval_sumstat2}  --out ${out} --rs_header ${params.head_rs_sumstat2} $se $beta $z  --bfile  $bfile $headn $ncount $freq
+    """
+}
+
+
+
 
 
 process plink_sumstat1_rep{
     cpus max_plink_cores
     memory plink_mem_req
   input :
-      file(filegwas) from file_plk_ss1_rep
-      file(bed_pos) from sumstat2_bed 
-      set file(bed), file(bim), file(fam) from raw_src_ch
+      path(filegwas) from file_plk_ss1_rep
+      path(bed_pos) from sumstat2_bed 
+      tuple path(bed), path(bim), path(fam) from raw_src_ch
     publishDir "${params.output_dir}/clump/",  overwrite:true, mode:'copy'
     output :
-       file("${out}.clumped") into clumped_sumstat1, clumped_sumstat1_2
-       file("${out}.log")
+       path("${out}.clumped") into clumped_sumstat1, clumped_sumstat1_2
+       path("${out}.log")
     script :
     out=params.output+"_subplk"
     plkf=bed.baseName
     """
-    plink -bfile $plkf --extract range ${bed_pos}  -out $out  --keep-allele-order --threads $max_plink_cores  --memory  $plink_mem_req_max  --clump $filegwas --clump-p1 ${params.clump_p1} --clump-p2 ${params.clump_p2} --clump-r2 ${params.clump_r2} --clump-kb ${params.clump_kb}
+    plink -bfile $plkf --extract range ${bed_pos}  -out $out  --keep-allele-order --threads $max_plink_cores  --memory  $plink_mem_req_max  --clump $filegwas --clump-p1 ${params.clump_p1} --clump-p2 ${params.clump_p2} --clump-r2 ${params.clump_r2} --clump-kb ${params.clump_kb} 
     """
 }
 
@@ -211,24 +256,20 @@ filegwas_sum2_stat=Channel.fromPath(params.file_gwas_sumstat2,checkIfExists:true
 process computed_stat{
   label 'R'
   input :
-    file(clump) from clumped_sumstat1 
-    file(sumstat1) from filegwas_sum1_stat
-    file(sumstat2) from filegwas_sum2_stat
+    path(clump) from clumped_sumstat1 
+    path(sumstat1) from fileformat_plk1 
+    path(sumstat2) from fileformat_plk2
   publishDir "${params.output_dir}/tmp",  overwrite:true, mode:'copy', pattern: "${out}*"
   output :
-    file("$out*")
-    file("${out}_all_clump.csv") into resume_all
-    set file('sumstat_ref.metal'), file('sumstat_cmp.metal') into metalanalyse_ch
+    path("$out*")
+    path("${out}_all_clump.csv") into resume_all
+    tuple path('sumstat_ref.metal'), path('sumstat_cmp.metal') into metalanalyse_ch
  script :
    out=params.output+"_stat" 
-   n_ref=(params.head_n_sumstat1!="") ? " --gwas_ref_n ${params.head_n_sumstat1} " :  " --gwas_ref_ncount ${params.n_count1}"
-   n_cmp=(params.head_n_sumstat2!="") ? " --gwas_cmp_n ${params.head_n_sumstat2} " :  " --gwas_cmp_ncount ${params.n_count2}"
+   gwasref="--gwas_ref $sumstat1 --gwas_ref_chr CHR --gwas_ref_bp BP --gwas_ref_a1 A1 --gwas_ref_a2 A2 --gwas_ref_beta BETA --gwas_ref_se SE --gwas_ref_p P --gwas_ref_af AF "
+   gwascmp="--gwas_cmp $sumstat2 --gwas_ref_chr CHR --gwas_ref_bp BP --gwas_ref_a1 A1 --gwas_ref_a2 A2 --gwas_ref_beta BETA --gwas_ref_se SE --gwas_ref_p P --gwas_ref_af AF "
  """ 
-#
- extract_pvalue.r \
- --gwas_ref $sumstat1 --gwas_ref_chr ${params.head_chr_sumstat1} --gwas_ref_bp ${params.head_bp_sumstat1} --gwas_ref_a1 ${params.head_A1_sumstat1} --gwas_ref_a2 ${params.head_A2_sumstat1} --gwas_ref_beta ${params.head_beta_sumstat1} --gwas_ref_se ${params.head_se_sumstat1} --gwas_ref_p ${params.head_pval_sumstat1} --gwas_ref_af ${params.head_freq_sumstat1} \
- --gwas_cmp $sumstat2 --gwas_cmp_chr ${params.head_chr_sumstat2} --gwas_cmp_bp ${params.head_bp_sumstat2} --gwas_cmp_a1 ${params.head_A1_sumstat2} --gwas_cmp_a2 ${params.head_A2_sumstat2} --gwas_cmp_beta ${params.head_beta_sumstat2} --gwas_cmp_se ${params.head_se_sumstat2} --gwas_cmp_p ${params.head_pval_sumstat2} --gwas_cmp_af ${params.head_freq_sumstat2} \
- --file_clump $clump --out $out $n_ref $n_cmp
+  extract_pvalue.r $gwasref $gwascmp --file_clump $clump --out $out $n_ref $n_cmp
   """
 }
 
@@ -236,11 +277,11 @@ process computed_stat{
 process do_metal{
   label 'metaanalyse'
   input :
-    set file(sumstat1), file(sumstat2) from metalanalyse_ch
+    tuple path(sumstat1), path(sumstat2) from metalanalyse_ch
   publishDir "${params.output_dir}/tmp/metal",  overwrite:true, mode:'copy'
   output :
-     file("${out}_1.stat") into metal_res_ch
-     file("${metal_config}")
+     path("${out}_1.stat") into metal_res_ch
+     path("${metal_config}")
   script :
       metal_config="metal_config.config"
       out=params.output+'_metal'
@@ -255,11 +296,11 @@ process do_metal{
 process merge_res{
   label 'R'
   input  :
-     file(metal) from metal_res_ch
-     file(fileres) from resume_all
+     path(metal) from metal_res_ch
+     path(fileres) from resume_all
   publishDir "${params.output_dir}/",  overwrite:true, mode:'copy'
   output : 
-     file(res)
+     path(res)
   script :
     res=params.output+'_all.csv'
     """
