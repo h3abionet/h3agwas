7c365b71fc8991ed7e8b0b703aa69fcac2e3f9cb
jeantristanb <jeantristanb@free.fr>
2023-04-24 08:47:40 +0200

    minor bug

--- formatdata/bin/change_updateidplink.r
+++ formatdata/bin/change_updateidplink.r
@@ -12,15 +12,15 @@ sameFIDIID<-merge(Data,Fam, by=c(1,2), all=F);nsameFIDIID<-nrow(sameFIDIID)
 
 ##case 2 : information just on ID  (FID)
 Fam2<-Fam;Fam2$FID<-Fam2$IID
-sameIID<-merge(Data,Fam2, by=c(1,2), all=F);nsameIID<-nrow(sameIID)
-head(sameIID)
+SameIID<-merge(Data,Fam2, by=c(1,2), all=F);nSameIID<-nrow(SameIID)
+head(SameIID)
 
 ##case 3 =>  IID : %FID_%IID
 Fam3<-Fam;Fam3$FID<-sapply(strsplit(as.character(Fam2$IID),split='_'),function(x)x[1]);Fam3$IID<-sapply(strsplit(as.character(Fam2$IID),split='_'),function(x)x[2]) 
 CompositFIDIID<-merge(Data,Fam3, by=c(1,2), all=F);nCompositFIDIID<-nrow(CompositFIDIID)
 head(CompositFIDIID)
 
-if(nsameFIDIID+nsameIID+nCompositFIDIID==0){
+if(nsameFIDIID+nSameIID+nCompositFIDIID==0){
 cat('--------------------------- FAM ----------------')
 print(head(Fam[,c(1,2)], 25))
 cat('--------------------------- Data ----------------')
@@ -29,7 +29,7 @@ cat('problem no name corresponding between fam file and data')
 quit("no", 5)
 }
 
-bestsol<-which.max(c(nsameFIDIID,nsameIID,nCompositFIDIID))
+bestsol<-which.max(c(nsameFIDIID,nSameIID,nCompositFIDIID))
 head(CompositFIDIID)
 if(bestsol==1)write.table(sameFIDIID[,c('FID_I','IID_I', 'FID','IID')], file=args[3], row.names=F, col.names=F, quote=F, sep='\t')
 if(bestsol==2)write.table(SameIID[,c('FID_I','IID_I', 'FID','IID')], file=args[3], row.names=F, col.names=F, quote=F, sep='\t')
